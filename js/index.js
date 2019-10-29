e44806c1bcafc9524f95b15a079716ea6c7d860b
lukasjaronis <jaronisluke13@gmail.com>
2019-10-29 15:23:27 -0400

    added a new anchor element to nav

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+
+
+// added an anchor tag to nav
+let nav = document.querySelector("nav");
+
+let nightShift = document.createElement("a"); 
+let nightShiftText = document.createTextNode("Night Shift");
+nightShift.prepend(nightShiftText);
+nightShift.style.color = "red";
+nightShift.style.fontWeight = "900";
+nav.appendChild(nightShift);
+
+nightShift.addEventListener('mouseover' () => {
+
+let 
+
+});
